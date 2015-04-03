83348f34cd742941db460e8c43f59e5703fec0d4
Tony DiNitto <think41c@gmail.com>
2015-04-02 23:29:55 -0600

    Pseudo code added

--- bob.rb
+++ bob.rb
@@ -4,4 +4,10 @@ class Bob
     "Whatever."
   end
 
+# Psuedo-code
+# If upcasing the input is equal to the input, then the comment is shouting and should result in "Woah, chill out!" 
+# If input is junky, like blank lines, tabs, etc. then give a "Fine. be that way."
+# If ? is the last character of a string, then output "Sure."
+# If Not a ? and not all upcase, and not blank (eg. 'else') then output "Whatever"
+
 end
